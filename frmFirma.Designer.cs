1ebf982ca3bc1a3c402049f14e45a3b472df1a7d
Lucas Valinotti <lucasvalinotti31@gmail.com>
2024-04-01 01:25:03 -0300

    Optimice el tema del dibujo e implemente el guardado de la firma.

--- frmFirma.Designer.cs
+++ frmFirma.Designer.cs
@@ -29,29 +29,25 @@
         private void InitializeComponent()
         {
             this.gbHerramientas = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdBorrar = new System.Windows.Forms.Button();
             this.pbColor = new System.Windows.Forms.PictureBox();
             this.cmdGuardar = new System.Windows.Forms.Button();
-            this.nudGrosor = new System.Windows.Forms.NumericUpDown();
-            this.lblGrosor = new System.Windows.Forms.Label();
-            this.cmdColor = new System.Windows.Forms.Button();
             this.gbDibujo = new System.Windows.Forms.GroupBox();
             this.pbDibujo = new System.Windows.Forms.PictureBox();
             this.cdColor = new System.Windows.Forms.ColorDialog();
-            this.sfGuardarFirma = new System.Windows.Forms.SaveFileDialog();
             this.gbHerramientas.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbColor)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudGrosor)).BeginInit();
             this.gbDibujo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbDibujo)).BeginInit();
             this.SuspendLayout();
             // 
             // gbHerramientas
             // 
+            this.gbHerramientas.Controls.Add(this.label1);
+            this.gbHerramientas.Controls.Add(this.cmdBorrar);
             this.gbHerramientas.Controls.Add(this.pbColor);
             this.gbHerramientas.Controls.Add(this.cmdGuardar);
-            this.gbHerramientas.Controls.Add(this.nudGrosor);
-            this.gbHerramientas.Controls.Add(this.lblGrosor);
-            this.gbHerramientas.Controls.Add(this.cmdColor);
             this.gbHerramientas.Location = new System.Drawing.Point(12, 12);
             this.gbHerramientas.Name = "gbHerramientas";
             this.gbHerramientas.Size = new System.Drawing.Size(142, 426);
@@ -59,14 +55,35 @@
             this.gbHerramientas.TabStop = false;
             this.gbHerramientas.Text = "Herramientas";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Seleccionar Color";
+            // 
+            // cmdBorrar
+            // 
+            this.cmdBorrar.Location = new System.Drawing.Point(16, 368);
+            this.cmdBorrar.Name = "cmdBorrar";
+            this.cmdBorrar.Size = new System.Drawing.Size(109, 23);
+            this.cmdBorrar.TabIndex = 2;
+            this.cmdBorrar.Text = "Borrar Lienzo";
+            this.cmdBorrar.UseVisualStyleBackColor = true;
+            this.cmdBorrar.Click += new System.EventHandler(this.cmdBorrar_Click);
+            // 
             // pbColor
             // 
             this.pbColor.BackColor = System.Drawing.Color.Black;
-            this.pbColor.Location = new System.Drawing.Point(16, 68);
+            this.pbColor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbColor.Location = new System.Drawing.Point(7, 43);
             this.pbColor.Name = "pbColor";
             this.pbColor.Size = new System.Drawing.Size(109, 53);
             this.pbColor.TabIndex = 1;
             this.pbColor.TabStop = false;
+            this.pbColor.Click += new System.EventHandler(this.pbColor_Click);
             // 
             // cmdGuardar
             // 
@@ -77,47 +94,6 @@
             this.cmdGuardar.Text = "Guardar Firma";
             this.cmdGuardar.UseVisualStyleBackColor = true;
             this.cmdGuardar.Click += new System.EventHandler(this.cmdGuardar_Click);
-            // 
-            // nudGrosor
-            // 
-            this.nudGrosor.Location = new System.Drawing.Point(16, 170);
-            this.nudGrosor.Maximum = new decimal(new int[] {
-            10,
-            0,
-            0,
-            0});
-            this.nudGrosor.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.nudGrosor.Name = "nudGrosor";
-            this.nudGrosor.Size = new System.Drawing.Size(109, 23);
-            this.nudGrosor.TabIndex = 0;
-            this.nudGrosor.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            // 
-            // lblGrosor
-            // 
-            this.lblGrosor.AutoSize = true;
-            this.lblGrosor.Location = new System.Drawing.Point(16, 152);
-            this.lblGrosor.Name = "lblGrosor";
-            this.lblGrosor.Size = new System.Drawing.Size(79, 15);
-            this.lblGrosor.TabIndex = 0;
-            this.lblGrosor.Text = "Grosor (1-10):";
-            // 
-            // cmdColor
-            // 
-            this.cmdColor.Location = new System.Drawing.Point(16, 39);
-            this.cmdColor.Name = "cmdColor";
-            this.cmdColor.Size = new System.Drawing.Size(109, 23);
-            this.cmdColor.TabIndex = 0;
-            this.cmdColor.Text = "Seleccionar Color";
-            this.cmdColor.UseVisualStyleBackColor = true;
-            this.cmdColor.Click += new System.EventHandler(this.cmdColor_Click);
             // 
             // gbDibujo
             // 
@@ -140,12 +116,6 @@
             this.pbDibujo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbDibujo_MouseDown);
             this.pbDibujo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbDibujo_MouseMove);
             // 
-            // sfGuardarFirma
-            // 
-            this.sfGuardarFirma.Filter = "Archivos PNG (*.png)|*.png";
-            this.sfGuardarFirma.InitialDirectory = ".";
-            this.sfGuardarFirma.Title = "Guardar Firma";
-            // 
             // frmFirma
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -160,7 +130,6 @@
             this.gbHerramientas.ResumeLayout(false);
             this.gbHerramientas.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbColor)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudGrosor)).EndInit();
             this.gbDibujo.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbDibujo)).EndInit();
             this.ResumeLayout(false);
@@ -170,14 +139,12 @@
         #endregion
 
         private GroupBox gbHerramientas;
-        private NumericUpDown nudGrosor;
-        private Label lblGrosor;
-        private Button cmdColor;
         private GroupBox gbDibujo;
         private Button cmdGuardar;
         private PictureBox pbDibujo;
         private ColorDialog cdColor;
         private PictureBox pbColor;
-        private SaveFileDialog sfGuardarFirma;
+        private Button cmdBorrar;
+        private Label label1;
     }
 }
--- frmFirma.cs
+++ frmFirma.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,39 +15,105 @@ namespace pryValinotti
     public partial class frmFirma : Form
     {
         Graphics lienzo;
-        bool puedeDibujar = false;
+        List<Point> puntos = new List<Point>();
+        Bitmap bmpLienzo;
         public frmFirma()
         {
             InitializeComponent();
-            lienzo = pbDibujo.CreateGraphics();
-        }
-
-        private void cmdColor_Click(object sender, EventArgs e)
-        {
-            if (cdColor.ShowDialog() == DialogResult.OK)
-            {
-                pbColor.BackColor = cdColor.Color;
-            }
-        }
-
-        private void pbDibujo_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left) puedeDibujar = true;
-            else if (e.Button == MouseButtons.Right) puedeDibujar = false;
+            bmpLienzo = new Bitmap(pbDibujo.Width, pbDibujo.Height);
+            lienzo = Graphics.FromImage(bmpLienzo);
         }
 
         private void pbDibujo_MouseMove(object sender, MouseEventArgs e)
         {
-            Brush brush = new SolidBrush(pbColor.BackColor);
-            int grosor = Convert.ToInt32(nudGrosor.Value);
-            if (puedeDibujar) lienzo.FillEllipse(brush, e.X, e.Y, grosor, grosor);
+            Pen pen = new Pen(pbColor.BackColor, 3);
+            if (e.Button == MouseButtons.Left) 
+            {
+                try
+                {
+                // Agrego los puntos donde estuvo el mmouse para dibujar una linea
+                puntos.Add(e.Location);
+                lienzo.DrawCurve(pen, puntos.ToArray(), 0.5f);
+                pbDibujo.Image = bmpLienzo;
+                } catch (Exception ex) { MessageBox.Show("Intenta no presionar ambos botones al mismo tiempo :)"); }
+
+                // El siguiente codigo esta por las dudas llegue a ser mas eficiente, pero no
+                // lo es seguramente.
+                //
+                //if (puntos.Count > 2)
+                //{
+                //    int len = puntos.Count;
+                //    List<Point> subpuntos = new List<Point>();
+                //    subpuntos.Add(puntos[len - 1]);
+                //    subpuntos.Add(puntos[len - 2]);
+
+                //}
+            }
+            else
+            {
+                puntos.Clear();
+            }
         }
 
         private void cmdGuardar_Click(object sender, EventArgs e)
         {
-            if(sfGuardarFirma.ShowDialog() == DialogResult.OK)
+            // Convertir la imagen del PictureBox a un Bitmap
+            //  Un Bitmap es una representación en memoria de una imagen,
+            //  que incluye todos los píxeles de la imagen.
+            Bitmap bmp = (Bitmap)pbDibujo.Image;
+
+            // Crear un objeto SaveFileDialog
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            // Que tipos de archivo quiero guardar
+            sfd.Filter = "Imágenes (*.png)|*.png;";
+
+            // Titulo del filedialog
+            sfd.Title = "Guardar dibujo";
+
+            // Mostrar el diálogo
+            DialogResult result = sfd.ShowDialog();
+
+            // Si el usuario seleccionó un archivo
+            if (result == DialogResult.OK)
             {
-                return;
+                // Obtener el nombre del archivo
+                string filename = sfd.FileName;
+
+                // Guardar la imagen en formato PNG
+                bmp.Save(filename, ImageFormat.Png);
+                MessageBox.Show("Imagen guardada correctamente", "Guardar dibujo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // Liberar la memoria utilizada por el Bitmap
+            bmp.Dispose();
+        }
+
+        private void cmdBorrar_Click(object sender, EventArgs e)
+        {
+            lienzo.Clear(Color.White);
+            pbDibujo.Image = null;
+        }
+
+        private void pbDibujo_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                pbDibujo.Cursor = Cursors.Cross;
+                puntos.Add(e.Location);
+            }
+            //else if (e.Button == MouseButtons.Right)
+            //{
+            //    pbDibujo.Cursor = Cursors.Default;
+            //    puntos.Clear();
+            //}
+        }
+
+        private void pbColor_Click(object sender, EventArgs e)
+        {
+            if (cdColor.ShowDialog() == DialogResult.OK)
+            {
+                pbColor.BackColor = cdColor.Color;
             }
         }
     }
