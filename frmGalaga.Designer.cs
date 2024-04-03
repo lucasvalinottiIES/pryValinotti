b6f59519de82848a76d77654ae70fd61d1efc62b
Lucas Valinotti <lucasvalinotti31@gmail.com>
2024-04-03 18:18:08 -0300

    Agrego icono al juego

--- frmGalaga.Designer.cs
+++ frmGalaga.Designer.cs
@@ -118,6 +118,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pbPlayer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmGalaga";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
